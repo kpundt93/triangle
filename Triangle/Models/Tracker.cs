09a5c401b9c0a0d973565f849d40a6f3fb56bc89
Katie Pundt <kzpundt@gmail.com>
2021-12-07 09:30:09 -0800

    revised code for IsScalene()
    Co-Authored-by: Mike Watkins <michaelhugheswatkins@gmail.com>

--- Triangle/Models/Tracker.cs
+++ Triangle/Models/Tracker.cs
@@ -13,5 +13,23 @@ namespace Triangle
         return true;
       }
     }
+
+    public bool IsScalene(int side1, int side2, int side3)
+    {
+      if(IsTriangle(side1, side2, side3)){
+        if ((side1 != side2) && ((side1 != side3)) && ((side2 != side3)))
+        {
+          return true;
+        }
+        else
+        {
+          return false;
+        }
+      }
+      else
+      {
+        return false;
+      }
+    }
   }
 }
